f048e1fd1f2a9224ad375af186fb57f7ead7a7b4
Tamir Karssli <tkarssli@gmail.com>
2020-09-21 02:44:03 -0700

    disable bestbuy

--- index.ts
+++ index.ts
@@ -3,13 +3,13 @@ import { Logger, LogLevel } from "./src/Logger";
 import { DiscordNotification } from "./src/Notification/DiscordNotification";
 import { Evga } from "./src/Crawler/Evga";
 import { Nvidia } from "./src/Crawler/Nvidia";
-import { Bestbuy } from "./src/Crawler/Bestbuy";
+// import { Bestbuy } from "./src/Crawler/Bestbuy";
 
 require("dotenv").config();
 
 const bot = new Bot(
   (process.env.DELAY as unknown) as number,
-  [new Nvidia(), new Evga(), new Bestbuy()],
+  [new Nvidia(), new Evga()],
   [
     new DiscordNotification(
       process.env.DISCORD_WEBHOOK_ID as string,
